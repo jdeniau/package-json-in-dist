68f26b61c80a7db38618c11a293eec455f4dbe5e
Julien Deniau <julien.deniau@gmail.com>
2025-10-02 22:14:48 +0000

    Utility that copies and update package.json in dist folder to publish only this directory

---
+++ README.md
@@ -0,0 +1,13 @@
+# package-json-in-dist
+
+Utility that copies and update package.json in dist folder to publish only this directory
+
+## Usage
+
+```sh
+# default "dist/" directory
+npx package-json-in-dist
+
+# custom directory
+npx package-json-in-dist custom-dist
+```
---
+++ src/index.js
@@ -0,0 +1,61 @@
+import path from "node:path";
+import fs from "node:fs";
+
+const DEFAULT_DIST_DIRECTORY = process.argv[2] ?? "dist/";
+
+const suffixBySlash = (dir) => (dir.endsWith("/") ? dir : dir + "/");
+
+const DIST_DIRECTORY = suffixBySlash(DEFAULT_DIST_DIRECTORY);
+
+const packageJsonPath = path.resolve("package.json");
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
+
+const newPackageJson = { ...packageJson };
+
+// make package public
+newPackageJson.private = false;
+
+// remove "dist" from "main" and "types"
+newPackageJson.main = newPackageJson.main.replace(DIST_DIRECTORY, "");
+newPackageJson.types = newPackageJson.types.replace(DIST_DIRECTORY, "");
+
+// handle "exports" key
+for (const key in newPackageJson.exports) {
+  // remove "development" key from exports, and of single entrypoint, then make the values simpler
+  if (newPackageJson.exports[key].development) {
+    delete newPackageJson.exports[key].development;
+  }
+
+  if (
+    Object.keys(newPackageJson.exports[key]).length === 1 &&
+    newPackageJson.exports[key].default
+  ) {
+    newPackageJson.exports[key] = newPackageJson.exports[key].default;
+  }
+
+  // remove "dist" from exports paths
+  if (typeof newPackageJson.exports[key] === "string") {
+    newPackageJson.exports[key] = newPackageJson.exports[key].replace(
+      DIST_DIRECTORY,
+      ""
+    );
+  } else {
+    for (const subKey in newPackageJson.exports[key]) {
+      newPackageJson.exports[key][subKey] = newPackageJson.exports[key][
+        subKey
+      ].replace(DIST_DIRECTORY, "");
+    }
+  }
+}
+
+// remove "files", "scripts", "devDependencies" keys
+delete newPackageJson.files;
+delete newPackageJson.scripts;
+delete newPackageJson.devDependencies;
+
+fs.mkdirSync(path.resolve(DIST_DIRECTORY), { recursive: true });
+
+fs.writeFileSync(
+  path.resolve(DIST_DIRECTORY, "package.json"),
+  JSON.stringify(newPackageJson, null, 2)
+);
